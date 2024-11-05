74b3c5daee6592d4ce722d7d0c8d591f377ab2c9
Viki Val <viktorko99@gmail.com>
2024-11-05 15:30:36 +0100

    :tada: proxieees

---
+++ proxies/playground/index.ts
@@ -0,0 +1,24 @@
+// import { Binary, TxCallData, TxFinalizedPayload } from "polkadot-api"
+import { addressOf, buildAccount, publicKeyOf } from "../../multicall/config/account"
+import { Chain, magicApi } from "../../multicall/config/api"
+
+// This is you account if you do not have one
+// use node run generate
+const myAccount = addressOf(publicKeyOf())
+console.log('My account:', myAccount)
+
+// We will use AssetHub Paseo, but you can pass any valid
+// AssetHub here
+const api = magicApi('ahpas')
+
+// 1. mint an nft using as_derivate 
+
+// 2. using proxy change metadata to ://
+
+// 5. construct remark
+
+// 6. build the batc
+
+// 7. await the TX 
+
+// 8. console log the tx
