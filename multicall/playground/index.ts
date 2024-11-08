43efc64469a0517fb678b6dfae6bbfcbee7051af
Viki Val <viktorko99@gmail.com>
2024-11-08 17:00:36 +0700

    🦛 moodeng approves

--- multicall/playground/index.ts
+++ multicall/playground/index.ts
@@ -8,6 +8,7 @@ import {
   mintNonFungible,
   sendNonFungibleTo,
 } from "../../config/calls";
+import { getNextCollectionId } from "../../config/queries"
 
 // This is you account if you do not have one
 // use node run generate
@@ -17,12 +18,18 @@ console.log("My account:", myAccount);
 // We will use AssetHub Paseo, but you can pass any valid
 // AssetHub here
 const { api, disconnect } = magicApi("ahpas");
-const collectionId = "1"; // TODO: fix by dev
+
+const collectionId = await getNextCollectionId({ api });
+
+if (!collectionId) {
+  throw new Error("No collection found");
+}
+
 // 1. create a new collection
 const collection = createCollection({ api });
 
 // 2. mint an nft
-const mint = mintNonFungible({ api }, collectionId);
+const mint = mintNonFungible({ api }, collectionId.toString());
 
 // 3. construct nft send
 const send = sendNonFungibleTo({ api }, myAccount);
