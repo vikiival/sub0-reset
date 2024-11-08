24e7dccbfac3d28649c836804c49c610d9ca1dbc
Viki Val <viktorko99@gmail.com>
2024-11-08 17:14:03 +0700

    :technologist: giving todo

--- config/calls.ts
+++ config/calls.ts
@@ -2,46 +2,55 @@ import { process } from "std-env"
 import { ApiPromise, CHAIN, Chain, magicApi } from "./api"
 import { Binary, TxCallData, TxFinalizedPayload } from "polkadot-api"
 
-export function makeRemark({ api }: ApiPromise, memo: string) { //: TxCallData {
+export function makeRemark({ api }: ApiPromise, memo: string): TxCallData {
 	const remark = Binary.fromText(memo)
 	// with `api` object construct a onchain remark 
 	// the call you are looking for is in the `System` pallet
+	// TODO: remove the throw statement and do return with a call like
+  // `return api.` and the call you are looking for
 	throw new Error('[UNIMPLEMENTED] makeRemark')
-	return void 0
 }
 
 // DEV: you need to implement the following functions
-export function createCollection({ api }: ApiPromise) { //: TxCallData {
+export function createCollection({ api }: ApiPromise): TxCallData {
 	// with `api` object construct a mint call
 	// the call you are looking for is in the `Assets` pallet
+	// TODO: remove the throw statement and do return with a call like
+  // `return api.` and the call you are looking for
 	throw new Error('[UNIMPLEMENTED] mintAnAsset')
-	return void 0
+	
 }
 
-export function mintAnAsset({ api }: ApiPromise) { //: TxCallData {
+export function mintAnAsset({ api }: ApiPromise): TxCallData {
 	const assetId = process.env.ASSET_ID
 	// with `api` object construct a mint call
 	// the call you are looking for is in the `Assets` pallet
-	throw new Error('[UNIMPLEMENTED] mintAnAsset')
-	return void 0
+	// TODO: remove the throw statement and do return with a call like
+  // `return api.` and the call you are looking for
+	throw new Error('[UNIMPLEMENTED] mintAnAsset')	
 }
 
-export function mintNonFungible({ api }: ApiPromise, _collectionId: string) { //: TxCallData {
+export function mintNonFungible({ api }: ApiPromise, collectionId: string): TxCallData {
 	// with `api` object construct a mint call
 	// the call you are looking for is in the `NFTs` pallet
-	throw new Error('[UNIMPLEMENTED] mintNonFungible')
-	return void 0
+	// TODO: remove the throw statement and do return with a call like
+  // `return api.` and the call you are looking for
+	throw new Error('[UNIMPLEMENTED] mintNonFungible')	
 }
 
-export function sendAssetTo({ api }: ApiPromise, recipient: string) { //: TxCallData {
+export function sendAssetTo({ api }: ApiPromise, recipient: string): TxCallData {
+	// TODO: remove the throw statement and do return with a call like
+  // `return api.` and the call you are looking for
 	throw new Error('[UNIMPLEMENTED] sendAssetTo')
 }
 
-export function sendNonFungibleTo({ api }: ApiPromise, recipient: string) { //: TxCallData {
+export function sendNonFungibleTo({ api }: ApiPromise, recipient: string): TxCallData {
+	// TODO: remove the throw statement and do return with a call like
+  // `return api.` and the call you are looking for
 	throw new Error('[UNIMPLEMENTED] sendNonFungibleTo')
 }
 
-export function makeBatch({ api }: ApiPromise, data: TxCallData | TxCallData[]) { //: TxCallData {
+export function makeBatch({ api }: ApiPromise, data: TxCallData | TxCallData[]): TxCallData {
 	throw new Error('[UNIMPLEMENTED] makeBatch')
 	// with `api` object construct a batch of calls
 	// which one you use it is up to you :)
@@ -57,6 +66,7 @@ export function makeBatch({ api }: ApiPromise, data: TxCallData | TxCallData[]) 
 export function submit<T>(
 	data: TxCallData | TxCallData[],
 	chain: Chain = CHAIN,
-) { //: Promise<TxFinalizedPayload> {
+): Promise<TxFinalizedPayload> {
 	// const api = magicApi(chain)
+	throw new Error('[UNIMPLEMENTED] submit')
 }
--- config/queries.ts
+++ config/queries.ts
@@ -3,5 +3,23 @@ import { ApiPromise } from "./api"
 export function getNextCollectionId({ api }: ApiPromise): Promise<number | undefined> {
   // with `api` object construct a query
   // the call you are looking for is in the `Nfts` pallet
+  // TODO: remove the throw statement and do return with a query like
+  // `return api.` and the call you are looking for
   throw new Error('[UNIMPLEMENTED] getNextCollectionId')
+}
+
+export function getCollection({ api }: ApiPromise, collectionId: number): Promise<any> {
+  // with `api` object construct a query
+  // the call you are looking for is in the `Nfts` pallet
+  // TODO: remove the throw statement and do return with a query like
+  // `return api.` and the call you are looking for
+  throw new Error('[UNIMPLEMENTED] getCollection')
+}
+
+export function getCollectionList({ api }: ApiPromise): Promise<any> {
+  // with `api` object construct a query
+  // the call you are looking for is in the `Nfts` pallet
+  // TODO: remove the throw statement and do return with a query like
+  // `return api.` and the call you are looking for
+  throw new Error('[UNIMPLEMENTED] getCollectionList')
 }
