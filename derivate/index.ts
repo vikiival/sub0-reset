a5b46db602da42c55bf391e69f4d994c029e6e09
Viki Val <viktorko99@gmail.com>
2024-11-09 17:16:33 +0700

    :technologist: thanks Joseph

--- derivate/index.ts
+++ derivate/index.ts
@@ -1,14 +1,13 @@
 import { blake2b as blake256 } from "@noble/hashes/blake2b"
-import { addressOf, publicKeyOf } from "../config/account"
+import { u16 } from "@polkadot-api/substrate-bindings"
 import { Binary } from "polkadot-api"
-
-const hex = (d: number) => `0x` + Number(d).toString(16).padStart(2, '0')
+import { addressOf, publicKeyOf } from "../config/account"
 
 export function derive(publicKey: Uint8Array, index: number) {
   const construct: Uint8Array = new Uint8Array([
     ...Binary.fromText('modlpy/utilisuba').asBytes(),
     ...publicKey,
-    ...Binary.fromHex(`${hex(index)}00`).asBytes() // it needs to have lentgh of 16-bits (2 bytes)
+    ...u16.enc(index) // it needs to have lentgh of 16-bits (2 bytes)
   ])
   
   const hash = blake256(construct, {
