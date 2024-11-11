ef0970e214ae24bf683ae3b994aa9f263a4380b9
Viki Val <viktorko99@gmail.com>
2024-11-11 14:11:28 +0700

    :zap: save to .env

--- generate/index.ts
+++ generate/index.ts
@@ -1,15 +1,24 @@
 import { ed25519 } from "@noble/curves/ed25519"
 import { addressOf, publicKeyOf } from "../config/account"
+import { write, read } from 'rc9'
 
-const privateKey = ed25519.utils.randomPrivateKey()
+const dotenv = read('.env')
+
+const hasPrivateKey = !!dotenv.PRIVATE_KEY 
+
+const privateKey = hasPrivateKey ? dotenv.PRIVATE_KEY : ed25519.utils.randomPrivateKey()
 const address = addressOf(ed25519.getPublicKey(privateKey))
 const hex = Buffer.from(privateKey).toString('hex');
 
 console.log('Your Polkadot account is:', address)
 console.log('Your private key is:', hex)
 
-console.warn('save to .env file:\n', `PRIVATE_KEY=${hex}`)
-
+if (hasPrivateKey) {
+  console.warn('SAVING to .env file:\n', `PRIVATE_KEY=${hex}`)
+  write({ PRIVATE_KEY: hex }, {
+    name: '.env',
+  })  
+}
 
 const pk = addressOf(publicKeyOf(hex))
 
